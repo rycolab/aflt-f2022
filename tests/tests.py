d45139b1cc3bbe0f61dac252e092d11f817f01d0
Daniel Vera Nieto <daniel.veranieto@inf.ethz.ch>
2022-03-23 16:04:50 +0100

    revers, accessible and coaccesiblee tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,11 @@
 from rayuela.fsa.fsa import FSA
 import pickle
 
-with open("tests/fsas/fsas.pkl", 'rb') as f:
+# TODO: 
+# - Examples
+# - Edge cases
+
+with open("tests/pickles/fsas.pkl", 'rb') as f:
     fsas = pickle.load(f)
 
 
@@ -12,9 +16,9 @@ def test_deterministic():
 
 
 def test_pushed():
-    with open("tests/fsas/pushed_fsas.pkl", 'rb') as f:
+    with open("tests/pickles/pushed_fsas.pkl", 'rb') as f:
         pushed_fsas = pickle.load(f)
-    with open("tests/fsas/pushed_results.pkl", 'rb') as f:
+    with open("tests/pickles/pushed_results.pkl", 'rb') as f:
         pushed_results = pickle.load(f)
 
     for gt_fsa, result in zip(pushed_fsas, pushed_results):
@@ -22,5 +26,26 @@ def test_pushed():
     
 
 def test_reverse():
-    for fsa in fsas:
-        assert fsa.reverse() == "Reverse"
+    
+    with open("tests/pickles/reversed_fsas_ascii.pkl", 'rb') as f:
+        reversed_asciis = pickle.load(f)
+    
+    for fsa, rfsa in zip(fsas, reversed_asciis):
+        assert fsa.reverse().__str__() == rfsa
+
+def test_accessible():
+    
+    with open("tests/pickles/accessible_states.pkl", 'rb') as f:
+        accessible_states_fsas = pickle.load(f)
+    
+    for fsa, accessible_states in zip(fsas, accessible_states_fsas):
+        assert fsa.accesible() == accessible_states
+
+def test_coaccessible():
+    
+    with open("tests/pickles/coaccessible_states.pkl", 'rb') as f:
+        coaccessible_states_fsas = pickle.load(f)
+    
+    for fsa, coaccessible_states in zip(fsas, coaccessible_states_fsas):
+        assert fsa.coaccesible() == coaccessible_states
+
