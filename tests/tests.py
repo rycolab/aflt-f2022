2f0a656483e341a7561b443a21f7c6cd10ed82c8
Daniel Vera Nieto <daniel.veranieto@inf.ethz.ch>
2022-03-23 12:19:13 +0100

    Added gitignore. Implemented push test

--- tests/tests.py
+++ tests/tests.py
@@ -4,8 +4,7 @@ import pickle
 with open("tests/fsas/fsas.pkl", 'rb') as f:
     fsas = pickle.load(f)
 
-with open("tests/fsas/pushed_fsas.pkl", 'rb') as f:
-    gt_pushed_fsas = pickle.load(f)
+
 
 def test_deterministic():
     for fsa in fsas:
@@ -13,10 +12,13 @@ def test_deterministic():
 
 
 def test_pushed():
-    pushed_fsas = [fsa.push() for fsa in fsas]
-    for gt_fsa, pushed_fsa in zip(gt_pushed_fsas, pushed_fsas):
-        assert pushed_fsa.pushed == True
-        assert gt_fsa.__str__() == pushed_fsa.__str__()
+    with open("tests/fsas/pushed_fsas.pkl", 'rb') as f:
+        pushed_fsas = pickle.load(f)
+    with open("tests/fsas/pushed_results.pkl", 'rb') as f:
+        pushed_results = pickle.load(f)
+
+    for gt_fsa, result in zip(pushed_fsas, pushed_results):
+        assert gt_fsa.pushed == result
     
 
 def test_reverse():
